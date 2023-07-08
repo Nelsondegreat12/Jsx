36538a056f938d7ee1dee60d3ee255b8482b1a87
Nelson <nelsondegreat12@gmail.com>
2023-07-08 14:26:44 +0100

    jsx

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FullName = () => 
+{
+let firstName = "Anthony";
+let middleName = "Nelson";
+let lastName = "Somtochukwu";
+
+return <p>{firstName + " " + middleName + " " + lastName}</p>;
+};
+
+export default FullName;
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProfilePhoto() {
+  return (
+    <div>
+      <img src= "/believe.jpg" className="rounded" alt="Profile Img" />
+    </div>
+  );
+}
+
+export default ProfilePhoto;
